06641863075e80ab039514ffa309903b2f7ac679
Abhijeet Parashar <31612672+parasharabhijeet@users.noreply.github.com>
2019-07-06 16:18:40 +0530

    Add files via upload

---
+++ 41A.cpp
@@ -0,0 +1,79 @@
+/*
+Hey there stalker....
+
+█▀▀▄░░░░░░░░░░░▄▀▀█
+░█░░░▀▄░▄▄▄▄▄░▄▀░░░█
+░░▀▄░░░▀░░░░░▀░░░▄▀
+░░░░▌░▄▄░░░▄▄░▐▀▀
+░░░▐░░█▄░░░▄█░░▌▄▄▀▀▀▀█
+░░░▌▄▄▀▀░▄░▀▀▄▄▐░░░░░░█
+▄▀▀▐▀▀░▄▄▄▄▄░▀▀▌▄▄▄░░░█
+█░░░▀▄░█░░░█░▄▀░░░░█▀▀▀
+░▀▄░░▀░░▀▀▀░░▀░░░▄█▀
+░░░█░░░░░░░░░░░▄▀▄░▀▄
+░░░█░░░░░░░░░▄▀█░░█░░█
+░░░█░░░░░░░░░░░█▄█░░▄▀
+░░░█░░░░░░░░░░░████▀
+░░░▀▄▄▀▀▄▄▀▀▄▄▄█▀
+
+Like what you see?
+*/
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define pii         pair<ll ,ll>
+#define vi          vector<ll>
+#define all(a)      (a).begin(),(a).end()
+#define F           first
+#define S           second
+#define sz(x)       (ll)x.size()
+#define rep(i,a,b)    for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define ll long long int
+#define lld long double
+#define endl "\n"
+#define MOD 1000000007
+int main()
+{
+   IOS
+
+    int TESTS=1;
+    //cin >> TESTS;
+
+    while (TESTS--)
+    {
+       string s,t;
+       cin>> s>> t;
+      ll l1=s.length();
+        ll l2=t.length();
+       if (l1!=l2)
+       {
+           cout << "NO"<<endl;
+       }
+       else
+       {
+           rep(i,0,sz(s))
+           {
+               if (s[i]!=t[sz(s)-i-1])
+               {
+                   cout << "NO"<<endl;
+                   return 0;
+               }
+
+           }
+           cout << "YES";
+       }
+    }
+return 0;
+}
+
+
